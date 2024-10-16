31e20820b9427b789dcafd9e3ecac7fc227c58e3
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-10-16 11:00:53 +0200

    feat(web): add injector/provider

--- lib/cli/index.ts
+++ lib/cli/index.ts
@@ -2,19 +2,26 @@ import { parseArgs } from "./src/parse-args.js";
 import compile from "./src/compile.js";
 import publish from "./src/publish.js";
 
+const commands = {
+  compile,
+  publish,
+};
+
 async function main() {
   const args = parseArgs();
   const command = args.args.shift();
+  const executor = commands[command];
 
-  if (command === "compile") {
-    compile(args);
-    return;
+  if (executor) {
+    try {
+      await executor(args);
+    } catch (error) {
+      console.error(String(error));
+      process.exit(1);
+    }
   }
 
-  if (command === "publish") {
-    publish(args);
-    return;
-  }
+  throw new Error("Invalid command: " + command);
 }
 
 main();
