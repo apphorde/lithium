fd393ee708f48428f2e1fa73bafb8358af48844b
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-11-29 11:00:53 +0000

    feat(store): add initial API

--- lib/store/index.ts
+++ lib/store/index.ts
@@ -1,6 +1,75 @@
-import { ref } from '@lithium/reactive';
-import type { Ref } from '@lithium/reactive';
+import { observer, ref } from "@lithium/reactive";
+import type { Ref } from "@lithium/reactive";
 
-export function useStore<T>(initialState: T) {
-  function select<V>(selector: (state: T) => V): Ref<V>
+const noop = () => {};
+
+export interface Action<T = any> {
+  type: string;
+  payload?: T;
+}
+
+export type Reducer<A, T> = (action: A, state: T) => T;
+export type Effect<A, T> = (action: A, state: T) => void | Promise<void>;
+
+export function useStore<T, A extends Action>(initialState: T) {
+  const events = new EventTarget();
+
+  function check() {
+    for (const f of observers) f();
+  }
+
+  const observers = [];
+  const reducers = [];
+  const effects = [];
+  const state = ref<T>(initialState, check);
+  const selectAll = (state) => state;
+
+  function select<V>(selector: (state: T) => V = selectAll): Ref<V> {
+    const v = selector(state.value);
+    const s = ref(v, noop);
+
+    const o = observer(
+      () => selector(state.value),
+      (value) => (s.value = value)
+    );
+
+    observers.push(o);
+
+    return s;
+  }
+
+  function dispatch(action: A) {
+    let next = structuredClone(state.value);
+
+    try {
+      for (const reducer of reducers) {
+        if (reducer[0] === action.type) {
+          next = reducer[1](next, action);
+        }
+      }
+
+      state.value = next;
+
+      for (const effect of effects) {
+        if (effect[0] === action.type) {
+          effect[1](next, action);
+        }
+      }
+    } catch (error) {
+      events.dispatchEvent(new ErrorEvent("error", { error: String(error) }));
+      return;
+    }
+
+    events.dispatchEvent(new CustomEvent("dispatch", { detail: action }));
+  }
+
+  function withReducer(type: string, fn: Reducer<A, T>) {
+    reducers.push([type, fn]);
+  }
+
+  function withEffect(type: string, fn: Effect<A, T>) {
+    effects.push([type, fn]);
+  }
+
+  return { events, select, dispatch, withReducer, withEffect };
 }
