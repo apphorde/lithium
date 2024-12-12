08f766bc37845a770e6015a3281e6d4df34045be
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-12-12 20:41:50 +0530

    refactor: split web package into layers

--- lib/web/src/layer-3/text-template.plugin.ts
+++ lib/web/src/layer-3/text-template.plugin.ts
@@ -4,26 +4,31 @@ import { plugins } from "../layer-0/plugin.js";
 import { watch } from "../layer-0/reactive.js";
 
 plugins.use({
-  createElement(_el, node: Text) {
+  createElement(node: Text) {
     if (node.nodeType !== node.TEXT_NODE) {
       return;
     }
 
-    const text = String(node.textContent);
-    if (!(text.includes("${") || text.includes("{{"))) {
-      return;
-    }
-
-    const expression =
-      "`" +
-      text.replace(
-        /\{\{([\s\S]+?)}}/g,
-        (_: any, inner: string) => "${ " + inner.trim() + " }"
-      ) +
-      "`";
-
-    node.textContent = "";
-    const fn = compileExpression(expression);
-    watch(wrapTryCatch(expression, fn), (v?: any) => setText(node, v));
+    createTextNodeBinding(node);
   },
 });
+
+function createTextNodeBinding(node: Text) {
+  const text = String(node.textContent);
+  if (!(text.includes("${") || text.includes("{{"))) {
+    return;
+  }
+
+  node.textContent = "";
+
+  const expression =
+    "`" +
+    text.replace(
+      /\{\{([\s\S]+?)}}/g,
+      (_: any, inner: string) => "${ " + inner.trim() + " }"
+    ) +
+    "`";
+
+  const fn = compileExpression(expression);
+  watch(wrapTryCatch(expression, fn), (v?: any) => setText(node, v));
+}
