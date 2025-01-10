c3bce337e4369ba27094cd333c9b7d282295c438
Darlan Alves <me@darlanalv.es>
2025-01-11 00:42:09 +0100

    chore: rebuild on workflow change

--- publish.mjs
+++ publish.mjs
@@ -2,14 +2,14 @@ import { execSync } from "child_process";
 import { readFileSync } from "fs";
 import { join } from "path";
 
-async function getRegistryVersion(packageName) {
+async function getRegistryVersions(packageName) {
   const response = await fetch(`https://registry.npmjs.org/${packageName}`);
   if (!response.ok || response.status > 399) {
     return "";
   }
 
   const data = await response.json();
-  return data["dist-tags"].latest;
+  return Object.keys(data.versions);
 }
 
 async function main() {
@@ -17,20 +17,23 @@ async function main() {
     const projectRoot = process.argv.slice(2)[0];
     const packageJson = JSON.parse(readFileSync(join(projectRoot, "package.json"), "utf8"));
     const { name, version } = packageJson;
-    const registryVersion = await getRegistryVersion(name);
 
     if (!version) {
       return;
     }
 
-    if (version !== registryVersion) {
-      console.log(`Publishing new version: ${version}`);
-      execSync("npm publish", { stdio: "inherit", cwd: projectRoot });
-    } else {
+    const registryVersions = await getRegistryVersions(name);
+
+    if (registryVersions.includes(version)) {
       console.log(`Version ${version} is already published.`);
+      return;
     }
+
+    console.log(`Publishing new version: ${version}`);
+    execSync(`cp .npmignore ${projectRoot}/`);
+    execSync("pnpm publish --git-checks false --access public", { stdio: "inherit", cwd: projectRoot });
   } catch (error) {
-    console.error("Error:", error);
+    console.error(error);
     process.exit(1);
   }
 }
