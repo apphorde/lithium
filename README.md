d2b73ff89c3d3e4106a7f346e513b489ff21f2a2
Darlan Alves <me@darlanalv.es>
2025-01-11 20:58:14 +0100

    chore: update documentation site

--- README.md
+++ README.md
@@ -1,33 +1,7 @@
-# Lithium Web Components
+# Lithium
 
-## Setup
+A batteries-included library to build reactive interfaces.
 
-For NPM users, add this to `.npmrc`
+## Documentation, examples and guides
 
-```text
-@li3:registry=https://npm.apphor.de
-```
-
-## @li3/web
-
-UI library for web apps using only standard HTML5 API's and modern ESM features.
-
-See [/web/README.md](web)
-
-`npm i @li3/web`
-
-## @li3/reactive
-
-The reactivity model used by `@li3/web`. Heavily inspired by all the work done by [VueJS](https://vuejs.org/)
-
-See [/reactive/README.md](reactive)
-
-`npm i @li3/web`
-
-## @li3/html-parser
-
-Self-explanatory
-
-See [/reactive/README.md](html-parser)
-
-`npm i @li3/html-parser`
+See [https://li3.dev](https://li3.dev) to get started
--- lib/web/src/index.ts
+++ lib/web/src/index.ts
@@ -1,30 +1,45 @@
 import { getOption, setOption } from "./internal-api/options.js";
 
-export * from "./internal-api/types.js";
-export * from "./internal-api/options.js";
-export * from "./internal-api/plugin.js";
-export * from "./internal-api/reactive.js";
-export * from "./internal-api/stack.js";
+export type * from "./internal-api/types.js";
+export { getOption, setOption } from "./internal-api/options.js";
+export { PluginDispatcher, plugins } from "./internal-api/plugin.js";
 
-export * from "./internal-api/dom.js";
+import * as reactive from "./internal-api/reactive.js";
+import * as stack from "./internal-api/stack.js";
+import * as dom from "./internal-api/dom.js";
+
 export * from "./component-api/setup.js";
-export * from "./component-api/inject.js";
+export { inject, provide } from "./component-api/inject.js";
+export { createComponent } from "./component-api/custom-elements.js";
+export { mount } from "./component-api/mount.js";
+export { bootstrap } from "./component-api/bootstrap.js";
 
-export * from "./component-api/custom-elements.js";
-export * from "./component-api/mount.js";
-export * from "./component-api/bootstrap.js";
-
-export * from "./plugins/property-binding.plugin.js";
-export * from "./plugins/event-handler.plugin.js";
-export * from "./plugins/inject-resources.plugin.js";
-export * from "./plugins/set-class.plugin.js";
-export * from "./plugins/set-element-ref.plugin.js";
-export * from "./plugins/set-style.plugin.js";
-export * from "./plugins/template-for.plugin.js";
-export * from "./plugins/template-if.plugin.js";
-export * from "./plugins/text-template.plugin.js";
+import * as plugin1 from "./plugins/property-binding.plugin.js";
+import * as plugin2 from "./plugins/event-handler.plugin.js";
+import * as plugin3 from "./plugins/inject-resources.plugin.js";
+import * as plugin4 from "./plugins/set-class.plugin.js";
+import * as plugin5 from "./plugins/set-element-ref.plugin.js";
+import * as plugin6 from "./plugins/set-style.plugin.js";
+import * as plugin7 from "./plugins/template-for.plugin.js";
+import * as plugin8 from "./plugins/template-if.plugin.js";
+import * as plugin9 from "./plugins/text-template.plugin.js";
 
 export { Ref, unref, isRef } from "@li3/reactive";
+
+export const internals = {
+  ...reactive,
+  ...stack,
+  ...dom,
+  ...plugin1,
+  ...plugin2,
+  ...plugin3,
+  ...plugin4,
+  ...plugin5,
+  ...plugin6,
+  ...plugin7,
+  ...plugin8,
+  ...plugin9,
+};
 
 window["Lithium"] = {
   setOption,
