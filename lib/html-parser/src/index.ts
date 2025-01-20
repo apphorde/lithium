f3c94a39ffc9136706678c1887fa6701ae5d247b
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2025-01-20 14:36:59 +0100

    feat(html-parser): add serialize function

--- lib/html-parser/src/index.ts
+++ lib/html-parser/src/index.ts
@@ -2,4 +2,5 @@
 export type { ParserAttribute, ParserNode, ChildNode, ElementNode, DocumentNode, TextNode, CommentNode } from './types';
 export type { PackedAttributes, PackedChildNode, PackedElementNode, PackedDocumentNode, PackedTextNode, PackedCommentNode } from './types';
 export { parse, Parser, pack, unpack } from './parse.js';
+export { serialize } from './serialize.js';
 export { compile } from './compile.js';
