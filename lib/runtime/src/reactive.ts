da28ed84e8a3ee15d2404a8c93d0f2ab0392b40f
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2025-02-18 14:20:30 +0000

    fix(runtime): state does not need watchDeep

--- lib/runtime/src/reactive.ts
+++ lib/runtime/src/reactive.ts
@@ -36,8 +36,7 @@ export function createState($el: RuntimeContext): void {
   const componentState = Object.assign(baseState, $el.setup($el) || {});
   Plugins.apply('setup', [$el, componentState]);
 
-  const combinedState = { ...$el.props, ...componentState };
-  $el.state = $el.reactive.watchDeep(combinedState);
+  $el.state = { ...$el.props, ...componentState };
   $el.stateKeys = Object.keys($el.state);
 
   if ($el.parent) {
