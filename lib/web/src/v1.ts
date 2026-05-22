ae873e95c2e4b9da92a3f0a3495d54cab496bc00
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-05-22 10:56:08 +0000

    chore: fix type issues from old file

--- lib/web/src/v1.ts
+++
@@ -1,6 +0,0 @@
-export { hostClasses, loadCss, loadScript, templateRef } from "@li3/plugins";
-export * from "@li3/browser";
-export * from "@li3/reactive";
-
-import { Component } from "@li3/browser";
-export const load = Component.loadAndParse;
