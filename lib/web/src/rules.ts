ef4571cd4a80af4388cbb753353758397520c1d9
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-06-25 02:12:27 +0000

    fix: remove weakref

--- lib/web/src/rules.ts
+++ lib/web/src/rules.ts
@@ -277,7 +277,7 @@ function updateForOfList(forNodes: any[], node: Node, key: string, indexKey: str
   const itemsToRemove = forNodes.slice(newLength);
 
   for (const next of itemsToRemove) {
-    suspend(next.item)x;
+    suspend(next.item);
     for (const node of next.nodes) {
       node.parentNode!.removeChild(node);
     }
